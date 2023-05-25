851e199b2f2f0af28c1421a11bacec8c797cf5b8
AntonMikhaylov1982 <134504199+AntonMikhaylov1982@users.noreply.github.com>
2023-05-25 23:14:09 +0300

    test

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
-
-
 from kivy.app import App
 from kivy.uix.label import Label
+
 #from kivy.uix.button import Button
+
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 #from kivy.uix.listview import ListView
